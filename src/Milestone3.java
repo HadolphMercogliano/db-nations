8ab7d1622427fa967ddce580631c8371b4b7ecc5
HadolphMercogliano <giovanni.mercogliano@gmail.com>
2023-06-16 17:32:19 +0200

    Bonus WIP

--- src/Milestone3.java
+++ src/Milestone3.java
@@ -16,16 +16,22 @@ public class Milestone3 {
       WHERE countries.name LIKE ?
       ORDER BY countries.name ASC;
       """;
+    String query2 = """
+      SELECT countries.name AS country_name, countries.country_id AS country_id, GROUP_CONCAT(languages.language) AS languages
+      FROM `countries`
+      JOIN country_languages ON country_languages.country_id = countries.country_id
+      JOIN languages ON country_languages.language_id = languages.language_id
+      WHERE countries.country_id = ?
+      GROUP BY countries.name, countries.country_id;
+      """;
     try (Connection conn = DriverManager.getConnection(url, user, password)){
       try (PreparedStatement ps = conn.prepareStatement(query)) {
-        // Chiedi all'utente di inserire il parametro di ricerca
         Scanner scanner = new Scanner(System.in);
         System.out.print("Cerca un paese: ");
         String param = scanner.nextLine();
         
-        // Imposta il parametro nella query
         ps.setString(1, "%" + param + "%");
-        scanner.close();
+        
         try (ResultSet rs = ps.executeQuery()) {
           while (rs.next()) {
             String country = rs.getString("country_name");
@@ -41,6 +47,28 @@ public class Milestone3 {
       } catch (SQLException e) {
         throw new RuntimeException(e);
       }
+      try (PreparedStatement ps2 = conn.prepareStatement(query2)) {
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.print("ID paese: ");
+        String param2 = scanner2.nextLine();
+        
+        ps2.setString(1, param2);
+        scanner2.close();
+        try (ResultSet rs2 = ps2.executeQuery()) {
+          while (rs2.next()) {
+            String country = rs2.getString("country_name");
+            String country_id = rs2.getString("country_id");
+            String languages = rs2.getString("languages");
+            
+            System.out.print("Country ID: " + country_id + ", ");
+            System.out.print("Country Name: "+ country + ", ");
+            System.out.print("Languages: "+ languages + "\n");
+          }
+        }
+      } catch (SQLException e) {
+        throw new RuntimeException(e);
+      }
+      
     } catch (SQLException e) {
       throw new RuntimeException(e);
     }
